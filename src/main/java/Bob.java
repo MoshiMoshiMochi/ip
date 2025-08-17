68361909e0c27512249ca966f85d0e9b0d2c2614
Thaddaeus Chua <thadchua@gmail.com>
2025-08-17 12:30:02 +0800

    Level 0: Changed name to Bob, Added skeletal version

---
+++ src/main/java/Bob.java
@@ -0,0 +1,19 @@
+public class Bob {
+    public static void main(String[] args) {
+        String logo =
+                "  ____    _____   ____  \n" +
+                " | __ )  |  _  | | __ ) \n" +
+                " |  _ \\  | | | | |  _ \\ \n" +
+                " | |_) | | |_| | | |_) |s\n" +
+                " |____/  |_____| |____/ \n";
+        String line =
+                "____________________________________________________________";
+        System.out.println("Hello from\n" + logo);
+        System.out.println(line);
+        System.out.println("Hows it bobbing dude?! I'm Bob");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye have a great time!");
+        System.out.println(line);
+    }
+}
