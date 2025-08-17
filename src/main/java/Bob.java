0b9957f7ba8a4d30641ce734c9aac8e1a3728587
Thaddaeus Chua <thadchua@gmail.com>
2025-08-17 14:06:14 +0800

    Level 1: Echo

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
         String logo =
@@ -8,10 +10,21 @@ public class Bob {
                 " |____/  |_____| |____/ \n";
         String line =
                 "____________________________________________________________";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(" Hello from\n" + logo);
         System.out.println(line);
-        System.out.println("Hows it bobbing dude?! I'm Bob");
-        System.out.println("What can I do for you?");
+        System.out.println(" Hows it bobbing dude?! I'm Bob");
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+        Scanner echo = new Scanner(System.in);
+        while (true){
+            String s = echo.nextLine();
+            if (s.toUpperCase().equals("BYE")) {
+                break;
+            } else {
+                System.out.println(line);
+                System.out.println(" " + s);
+            }
+        }
         System.out.println(line);
         System.out.println("Bye have a great time!");
         System.out.println(line);
