1926f6f98c46c1661707582b1b79ea0b6ac0f1f2
Thaddaeus Chua <thadchua@gmail.com>
2025-08-22 19:36:33 +0800

    Added JavaDoc comments

--- src/main/java/bob/command/FindCommand.java
+++ src/main/java/bob/command/FindCommand.java
@@ -6,13 +6,41 @@ import bob.task.Task;
 import bob.task.TaskList;
 import bob.ui.Ui;
 
+/**
+ * Represents a command that searches for tasks in the task list
+ * containing a given keyword in their description.
+ * <p>
+ * When executed, it filters tasks by iteratign throught the task
+ * list and checking if the keyword is present in their description,
+ * finally displaying all the tasks that matches the description
+ * provided to the user via the {@link Ui}.
+ * </p>
+ */
 public class FindCommand implements Command {
     private String description;
 
+    /**
+     * Constructs a {@code FindCommand} with the specified search keyword.
+     *
+     * @param description The keyword to search for within task descriptions.
+     */
     public FindCommand(String description) {
         this.description = description;
     }
 
+    /**
+     * Executes the find command by searching through the given {@link TaskList}
+     * for tasks whose descriptions contain the description specified during
+     * construction. Displays the results to the user through the {@link Ui}.
+     * <p>
+     * If no matching tasks is found, a message is shown indicating that no
+     * results were found. Otherwise, the list of matching tasks is displayed.
+     * </p>
+     *
+     * @param tasks     List of tasks to search within.
+     * @param ui        Ui instance for displaying messages.
+     * @param storage   The storage system (not used in this command).
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         TaskList matchingTasks = new TaskList();
@@ -48,6 +76,11 @@ public class FindCommand implements Command {
 
     }
 
+    /**
+     * Indicates whether this command causes the program to exit.
+     *
+     * @return {@code false}, since the find command does not exit the program.
+     */
     @Override
     public boolean isExit() {
         return false;
